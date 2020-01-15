a2e1a7f87dbeed97d1982006b28037c38082ba0a
yoanf <yoanfath@laposte.net>
2020-01-15 11:38:05 +0100

    Ajout form login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSliderModule} from '@angular/material';
+import {MatCardModule, MatInputModule, MatSliderModule} from '@angular/material';
 import { LoginComponent } from './pages/login/login.component';
 import 'hammerjs';
 import { NavComponent } from './pages/nav/nav.component';
@@ -30,7 +30,9 @@ import { MatListModule } from '@angular/material/list';
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
+    MatInputModule,
     MatIconModule,
+    MatCardModule,
     MatListModule,
   ],
   providers: [],
