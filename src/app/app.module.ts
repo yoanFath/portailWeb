10163ef76cf34eb4fa4446f4ce7ccbea82b69dc6
yoanf <yoanfath@laposte.net>
2020-01-22 12:11:18 +0100

    Ajout page actu, GED, dashboard et amélioration menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,16 +16,26 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import {ReactiveFormsModule} from '@angular/forms';
 import { UserCardComponent } from './components/user-card/user-card.component';
+import { DashboardComponent } from './pages/dashboard/dashboard/dashboard.component';
+import {RouterModule, Routes} from '@angular/router';
+import { GedComponent } from './pages/ged/ged.component';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { ActuComponent } from './pages/actu/actu.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     NavComponent,
-    UserCardComponent
+    UserCardComponent,
+    DashboardComponent,
+    GedComponent,
+    ActuComponent
   ],
   imports: [
     BrowserModule,
+    MatExpansionModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
@@ -35,6 +45,7 @@ import { UserCardComponent } from './components/user-card/user-card.component';
     MatButtonModule,
     MatSidenavModule,
     MatInputModule,
+    RouterModule,
     MatIconModule,
     MatCardModule,
     MatGridListModule,
